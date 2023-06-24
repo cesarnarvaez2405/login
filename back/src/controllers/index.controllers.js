508c850391cd89cdf0315fcd843b57b9a5e4ec51
cesarnarvaez2405 <cesar.teacher24@gmail.com>
2023-06-24 15:26:04 -0500

    Pull y push

--- back/src/controllers/index.controllers.js
+++ back/src/controllers/index.controllers.js
@@ -15,6 +15,11 @@ const getUser = async (req, res) => {
 }
 
 const createUser = async(req, res) => {
+
+    const {name, email} = req.body;
+    const response = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]); 
+    console.log(response)
+    res.send('Usuario creado')
     
 }
 
